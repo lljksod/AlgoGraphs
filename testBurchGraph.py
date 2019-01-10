3c79ff30fa82ac75a448a9694dee3a28e753e1ac
Richie Burch <rburch@gmail.com>
2019-01-09 21:07:49 -0600

    Cleaned up initial tests.

--- testBurchGraph.py
+++ testBurchGraph.py
@@ -9,34 +9,25 @@ class TestBurchGraph(unittest.TestCase):
 	def testCreateAbsGraph(self):
 		with self.assertRaises(TypeError):
 			t = BurchGraph()
-			del t
 		
 class TestBurchGraphAdjList(unittest.TestCase):
 	
 	def testCreateAdjListGraph(self):
 		t = BurchGraphAdjList()
-		print("first "+ str(t))
 		self.assertIsInstance(t,BurchGraphAdjList)
-		del t
 		
 	def testAddNode(self):
 		x = BurchGraphAdjList()
 		x.addNode(1)
-		print("second "+str(x))
 		self.assertIsInstance(x,BurchGraphAdjList)
 		self.assertEqual(x.payloads,{1:None})
 		self.assertEqual(x.neighbors(1),[])
-		del x
-
-class TestAddEdge(unittest.TestCase):
 	
 	def testAddEdge(self):
 		t = BurchGraphAdjList()
 		t.addNode(1)
 		t.addEdge(1,2)
-		print('third '+str(t))
 		self.assertEqual(t.payloads,{1:None,2:None})
-		del t
 		
 		
 	
